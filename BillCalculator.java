88d4f349230b235375d1462b203ff589e7aa3e92
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2025-01-20 08:51:45 +0530

    Delete BillCalculator.java

--- BillCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.billing.services;
-
-public class BillCalculator {
-    private static final double UNIT_RATE = 5.0; // Example rate per unit
-
-    public static double calculateBill(int unitsConsumed) {
-        return unitsConsumed * UNIT_RATE;
-    }
-}
